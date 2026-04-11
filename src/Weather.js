d99ea5e87a5a01f39da6d13cbb2baefadff6abf2
adeogunesther95-lab <adeogunesther95@gmail.com>
2026-04-11 06:29:30 +0100

    added container

--- src/Weather.js
+++ src/Weather.js
@@ -4,32 +4,36 @@ import React from "react";
 
 function Weather() {
   return (
-    <footer>
-      Coded by{" "}
-      <a
-        href="https://github.com/adeogunesther95-lab"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Esther Adeogun{" "}
-      </a>
-      on {""}
-      <a
-        href="https://github.com/adeogunesther95-lab/vanilla-weather-week8"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        GitHub {""}
-      </a>
-      hosted on {""}
-      <a
-        href="https://vanilla-weather-week8.netlify.app/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Netlify
-      </a>
-    </footer>
+    <div className="container">
+      <header></header>
+
+      <footer>
+        Coded by{" "}
+        <a
+          href="https://github.com/adeogunesther95-lab"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Esther Adeogun{" "}
+        </a>
+        on {""}
+        <a
+          href="https://github.com/adeogunesther95-lab/lesson-4-homework"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub {""}
+        </a>
+        hosted on {""}
+        <a
+          href="https://vanilla-weather-week8.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
+    </div>
   );
 }
 
